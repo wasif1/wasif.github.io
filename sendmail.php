eb4f083ad230685d5b1d8958dc254e858e3f59f2
waxif <waxif.1@gmail.com>
2024-09-07 20:17:55 +0400

    Download Resume Option added

--- sendmail.php
+++ sendmail.php
@@ -1,27 +1,27 @@
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(
-        !empty($_POST['name'])
-        && !empty($_POST['email'])
-        && !empty($_POST['message'])
-    ){
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $message = $_POST["message"];
-
-
-        $to = "waxif.1@gmail.com";
-        $subject = "New Contact Form Submission";
-        $body = "Name: {$name}\nEmail: {$email}\nMessage: {$message}";
-        $headers = "From: {$email}";
-
-
-        if (mail($to, $subject, $body, $headers)) {
-            echo "Message sent successfully!";
-        } else {
-            echo "Failed to send message.";
-        }
-    }
-}
-?>
+//
+// <?php
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     if(
+//         !empty($_POST['name'])
+//         && !empty($_POST['email'])
+//         && !empty($_POST['message'])
+//     ){
+//         $name = $_POST["name"];
+//         $email = $_POST["email"];
+//         $message = $_POST["message"];
+//
+//
+//         $to = "waxif.1@gmail.com";
+//         $subject = "New Contact Form Submission";
+//         $body = "Name: {$name}\nEmail: {$email}\nMessage: {$message}";
+//         $headers = "From: {$email}";
+//
+//
+//         if (mail($to, $subject, $body, $headers)) {
+//             echo "Message sent successfully!";
+//         } else {
+//             echo "Failed to send message.";
+//         }
+//     }
+// }
+// ?>
